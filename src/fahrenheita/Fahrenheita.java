f2d268ccf51264a9558272faeab410f891d19a19
TiM3er <33333118+TiM3er@users.noreply.github.com>
2017-11-11 21:57:04 +0100

    add Fahrenheit

--- src/fahrenheita/Fahrenheita.java
+++ src/fahrenheita/Fahrenheita.java
@@ -1,5 +1,12 @@
 package fahrenheita;
 
 public class Fahrenheita {
-
+	public static void FahrenheitaToCelsius(double Far)
+	{
+		System.out.println((Far-32)/1.8 + " C");
+	}
+	public static void FahrenheitaToKelvin(double Far)
+	{
+		System.out.println(((Far+459.67)*5)/9 + " K");
+	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,15 +1,28 @@
 package main;
 import Kelvin.Kelvin;
 import celsius.Celsius;
+import fahrenheita.Fahrenheita;;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		Kelvin.KelvinToCelsiusz(282.15);
-//		Kelvin.kelvinToFahrenheit(300);
-//		Celsius.CelsiusToKelvin(0);
-//		Celsius.CelsiusToFahrenheit(10);
-		
+
+
+	}
+	public static void Kelvin(double temp)
+	{
+		Kelvin.KelvinToCelsiusz(temp);
+		Kelvin.kelvinToFahrenheit(temp);	
+	}
+	public static void Celsius(double temp)
+	{
+		Celsius.CelsiusToKelvin(0);
+		Celsius.CelsiusToFahrenheit(10);
+	}
+	public static void Fahrenheita(double temp)
+	{
+		Fahrenheita.FahrenheitaToCelsius(0);
+		Fahrenheita.FahrenheitaToKelvin(60);	
 	}
 
 }
