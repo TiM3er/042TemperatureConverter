85bb6ba8caf4625eae1d527d263c1dc57dbdf2c9
TiM3er <33333118+TiM3er@users.noreply.github.com>
2017-11-11 21:44:33 +0100

    Add celsius

---
+++ src/celsius/Celsius.java
@@ -0,0 +1,12 @@
+package celsius;
+
+public class Celsius {
+	public static void CelsiusToKelvin(double Cel)
+	{
+		System.out.println(Cel+273.15 + " C");
+	}
+	public static void CelsiusToFahrenheit(double Cel)
+	{
+		System.out.println(32 + 1.8*Cel);
+	}
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,11 +1,15 @@
 package main;
 import Kelvin.Kelvin;
+import celsius.Celsius;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Kelvin.KelvinToCelsiusz(282.15);
-		Kelvin.kelvinToFahrenheit(300);
+//		Kelvin.KelvinToCelsiusz(282.15);
+//		Kelvin.kelvinToFahrenheit(300);
+//		Celsius.CelsiusToKelvin(0);
+//		Celsius.CelsiusToFahrenheit(10);
+		
 	}
 
 }
