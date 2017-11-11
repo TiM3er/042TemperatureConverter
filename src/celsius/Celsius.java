42801bc30519ea70d688fcf0c283286a7b944610
TiM3er <33333118+TiM3er@users.noreply.github.com>
2017-11-11 22:03:35 +0100

    add Fahrenheita and Ready

--- src/celsius/Celsius.java
+++ src/celsius/Celsius.java
@@ -3,10 +3,10 @@ package celsius;
 public class Celsius {
 	public static void CelsiusToKelvin(double Cel)
 	{
-		System.out.println(Cel+273.15 + " C");
+		System.out.println(Cel+273.15 + " K");
 	}
 	public static void CelsiusToFahrenheit(double Cel)
 	{
-		System.out.println(32 + 1.8*Cel);
+		System.out.println(32 + 1.8*Cel + " F");
 	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,6 @@
 package main;
+import java.util.Scanner;
+
 import Kelvin.Kelvin;
 import celsius.Celsius;
 import fahrenheita.Fahrenheita;;
@@ -6,21 +8,46 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		double temperatura;
+		System.out.println("C - celsius\nK - Kelvin\nF - Fahrenheit");
+		Scanner scan = new Scanner(System.in);
+		char wybur = scan.nextLine().charAt(0);
+		System.out.println("Give a Temperature to convert: ");
+		temperatura=scan.nextDouble();
+		switch (wybur) {
+		case 'K':
+			Kelvin(temperatura);
+		break;
+		case 'C':
+			Celsius(temperatura);
+		break;
+		case 'F':
+			Fahrenheita(temperatura);
+		break;
 
+		default:
+			Kelvin(temperatura);
+			Celsius(temperatura);
+			Fahrenheita(temperatura);
+		break;
+		}
 
 	}
 	public static void Kelvin(double temp)
 	{
+		System.out.println("From Kelvin to: ");
 		Kelvin.KelvinToCelsiusz(temp);
 		Kelvin.kelvinToFahrenheit(temp);	
 	}
 	public static void Celsius(double temp)
 	{
+		System.out.println("From Celsius to: ");
 		Celsius.CelsiusToKelvin(0);
 		Celsius.CelsiusToFahrenheit(10);
 	}
 	public static void Fahrenheita(double temp)
 	{
+		System.out.println("From Fahrenheita to: ");
 		Fahrenheita.FahrenheitaToCelsius(0);
 		Fahrenheita.FahrenheitaToKelvin(60);	
 	}
